201489210fbcd14022dcbb749ffdd35342a910df
jbscum01-hub <jb.scum01@gmail.com>
2026-01-27 05:50:03 +0700

    Update pool.js

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,7 +1,13 @@
 import pg from "pg";
 import { ENV } from "../config/env.js";
 
+const ssl =
+  ENV.DATABASE_URL.includes("sslmode=require") ||
+  ENV.DATABASE_URL.includes("neon.tech")
+    ? { rejectUnauthorized: false }
+    : false;
+
 export const pool = new pg.Pool({
   connectionString: ENV.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl,
 });
